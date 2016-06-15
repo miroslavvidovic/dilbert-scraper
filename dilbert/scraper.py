af09d2100cfb49cd84bf41ca38fe927c90345226
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-06-15 15:08:25 +0200

    Docstring and PEP 8 style

--- dilbert/scraper.py
+++ dilbert/scraper.py
@@ -1,42 +1,65 @@
-from bs4 import BeautifulSoup
-import requests
-from requests import get
+"""
+scraper.py module
+"""
 import time
 import os
+from bs4 import BeautifulSoup
+from requests import get
 
-class Scraper:
+
+class Scraper(object):
+    """
+    Scraper class
+    scrape the Dilbert comic website for useful data
+
+    """
 
     def __init__(self):
-        self.dilbert_page = requests.get('http://dilbert.com/')
+        self.dilbert_page = get('http://dilbert.com/')
         self.bsoup = BeautifulSoup(self.dilbert_page.content, "lxml")
 
     def get_data(self):
-        comics = self.bsoup.find_all('div',{'class':'comic-item-container'})
+        """
+          Scrape the data from the website
+          :return: data in dictionary format
+
+          """
+
+        def download(url, file_name):
+            """
+            Download a file
+            :param url: url of the targeted file
+            :param file_name: name for the saved file
+
+            """
+            path = "images/"
+            with open(os.path.join(path, file_name), "wb") as destination:
+                response = get(url)
+                destination.write(response.content)
+
+        def generate_name():
+            """
+            Generate an image file name with a timestamp
+            :return: timestamp.jpg
+            """
+            timestamp = time.asctime()
+            name = timestamp + ".jpg"
+            return name
+
+        comics = self.bsoup.find_all('div', {'class': 'comic-item-container'})
         output = []
         for comic in comics:
-            date = comic.find('span',{'class':'comic-date-wrapper'})
+            date = comic.find('span', {'class': 'comic-date-wrapper'})
             date_start = date.find('span').text
-            year = date.find('span',{'itemprop':'copyrightYear'}).text
+            year = date.find('span', {'itemprop': 'copyrightYear'}).text
 
-            img = comic.find('img',{'class':'img-comic'})
+            img = comic.find('img', {'class': 'img-comic'})
             url = img.attrs['src']
-            self.download(url, self.generate_name())
+            download(url, generate_name())
 
-            title = comic.find('span',{'class':'comic-title-name'}).text
-            output.append({'title':title,'date':date_start + year, 'url':url})
+            title = comic.find('span', {'class': 'comic-title-name'}).text
+            output.append({
+                'title': title, 'date': date_start + year, 'url': url
+            })
 
         return output
-
-    def download(self, url, file_name):
-        path = "images/"
-        with open(os.path.join(path, file_name), "wb") as file:
-            # get request
-            response = get(url)
-            # write to file
-            file.write(response.content)
-
-    def generate_name(self):
-        timestamp = time.asctime()
-        name = timestamp + ".jpg"
-        return name
-
--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from dilbert.scraper import Scraper
-from dilbert.scraper_new import Scraper
 from dilbert.dilbert_comic import DilbertComic
-
-'''
-scraper = Scraper()
-titles = scraper.find_titles()
-dates = scraper.find_dates()
-images = scraper.find_images()
-'''
 
 scraper = Scraper()
 result = scraper.get_data()
-print result
+print(result)
